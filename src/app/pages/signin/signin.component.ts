6156c30cbc79bc394d92a0877f0c7e1ce0f0e758
Karie Funk <kfunk@my365.bellevue.edu>
2020-04-07 17:53:09 -0600

    adding session & sign in

--- src/app/pages/signin/signin.component.ts
+++ src/app/pages/signin/signin.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-
-  constructor() { }
+  apiBaseUrl = `${environment.baseUrl}/api`;
+  form: FormGroup;
+  constructor(
+    private router: Router,
+    private cookieService: CookieService,
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {
+   
   }
+  
 
 }
